3a939f9171a910fb886b82611e790fb237c4068b
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2016-06-23 19:54:48 +0800

    updated lib versions

--- src/leiningen/new/webapp/project.clj
+++ src/leiningen/new/webapp/project.clj
@@ -1,17 +1,17 @@
 (defproject {{ns-name}} "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring "1.3.1"]
-                 [ring/ring-json "0.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [compojure "1.2.0"]
-                 [cheshire "5.3.1"]
-                 [prone "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring "1.5.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [compojure "1.5.1"]
+                 [cheshire "5.6.2"]
+                 [prone "1.1.1"]
                  ]
   :main {{ns-name}}.core
-  :plugins [[lein-ring "0.8.12"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler {{ns-name}}.core/app :auto-reload? true :auto-refresh? true}
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [ring-mock "0.1.5"]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
+                                  [ring/ring-mock "0.3.0"]
                                   ]}})
   
